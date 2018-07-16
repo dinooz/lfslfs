a5a2ea31bbb69d998a9c54ccd3b02cf250741e09
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-16 10:34:44 -0500

    Add files via upload

---
+++ lfslfs_download_files.sh
@@ -0,0 +1,30 @@
+export LFS=/mnt/lfs
+
+mkdir -pv $LFS
+mount -v -t ext4 /dev/sda2 $LFS
+mkdir -v $LFS/sources
+chmod -v a+wt $LFS/sources
+
+# http://www.linuxfromscratch.org/lfs/view/stable/chapter03/introduction.html
+cd $LFS/sources
+
+wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
+
+# wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+
+pushd $LFS/sources
+md5sum -c md5sums
+popd
+
+md5sum -c md5sums |grep FAILED
+
+mkdir -v $LFS/tools
+ln -sv $LFS/tools /
+
+groupadd lfs
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+
+passwd lfs
+chown -v lfs $LFS/tools
+chown -v lfs $LFS/sources
+
---
+++ lfslfs_reqs_install.sh
@@ -0,0 +1,14 @@
+dpkg-reconfigure dash
+apt -y install bison
+apt -y install gcc
+apt -y install g++
+apt -y install m4
+apt -y install make
+apt -y install makeinfo
+apt -y install texinfo
+
+apt -y install gawk
+apt -y install bison
+apt -y install automake
+
+
