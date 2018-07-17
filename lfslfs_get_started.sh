61cda913d5fb3087505c4f1904ab01f07479e475
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-17 10:44:35 -0500

    LFSLFS Get Started

---
+++ lfslfs_get_started.sh
@@ -0,0 +1,15 @@
+# Lets Download all our Scripts
+
+
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_initial_ubuntu.sh
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_parted.sh
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_reqs_check.sh
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_reqs_install.sh
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfs_get_files.sh
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_temporary_system.sh
+
+
+
+
+
+chmod 755 lfslfs*.sh
