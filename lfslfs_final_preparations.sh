e08cefbd2bb314d626f4e9bfce011e27c5e9cc45
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-17 11:10:17 -0500

    Tools and LFS User preparations

---
+++ lfslfs_final_preparations.sh
@@ -0,0 +1,12 @@
+# http://linuxfromscratch.org/lfs/view/stable/chapter04/creatingtoolsdir.html
+mkdir -v $LFS/tools
+ln -sv $LFS/tools /
+
+groupadd lfs
+useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+
+chown -v lfs $LFS/tools
+chown -v lfs $LFS/sources
+
+passwd lfs
+su - lfs
