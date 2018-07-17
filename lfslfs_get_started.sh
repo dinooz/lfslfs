c09d44231b1812b131e37c16f0cd3a2b291c05d6
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-17 11:22:00 -0500

    Update lfslfs_get_started.sh

--- lfslfs_get_started.sh
+++ lfslfs_get_started.sh
@@ -5,9 +5,9 @@ wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_parted.sh
 wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_reqs_check.sh
 wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_reqs_install.sh
 wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_get_files.sh
-wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_temporary_system.sh
 wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_final_preparations.sh
 wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_setup_lfs.sh
+wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_temporary_system.sh
 wget https://raw.githubusercontent.com/dinooz/lfslfs/master/lfslfs_system_chroot.sh
 
 
