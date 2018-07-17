a92dfe20b3b613d7adcb5e396c80b05b68a78f4f
Bernardino Lopez <bernardino_lopez@yahoo.com>
2018-07-17 12:50:30 -0500

    Include MD5Sum

--- lfslfs_get_files.sh
+++ lfslfs_get_files.sh
@@ -10,6 +10,7 @@ cd $LFS/sources
 wget http://www.linuxfromscratch.org/lfs/view/stable/wget-list
 
 wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+wget http://www.linuxfromscratch.org/lfs/view/stable/md5sums
 
 pushd $LFS/sources
 md5sum -c md5sums
